f0af8aa9683335408e55d32f4bc1b85a1aa57c88
buzzy <daniele.testa@gmail.com>
2019-07-12 21:44:17 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,26 @@
 set -e
 set -x
 
+#FETCH NEEDED TOOLS
+apt-get install -y gcc-8-aarch64-linux-gnu gcc-8-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc vboot-kernel-utils libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config
 
+#CREATE DIR STRUCTURE
+rm -fr /opt/sysroot/*
+cp -rv /opt/PowerOS/sysroot/* /opt/sysroot
+
+#GET WIFI RULES DATABASE
+cd /opt
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/linville/wireless-regdb.git
+
+#KERNEL
+cd /opt
+ln -s /usr/bin/aarch64-linux-gnu-gcc-8 /usr/bin/aarch64-linux-gnu-gcc
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+export WIFIVERSION=
+if [ ! -d "/opt/kernel" ]; then
+  wget -O /opt/kernel.tar.gz https://chromium.googlesource.com/chromiumos/third_party/kernel/+archive/86596f58eadf.tar.gz
+  mkdir /opt/kernel
+  tar xfv /opt/kernel.tar.gz -C /opt/kernel
+fi
+cd /opt/kernel
