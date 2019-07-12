3542f3d9b5c414a93cdfdc9c5af33d65ec96270b
buzzy <daniele.testa@gmail.com>
2019-07-12 21:50:10 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,3 +25,12 @@ if [ ! -d "/opt/kernel" ]; then
   tar xfv /opt/kernel.tar.gz -C /opt/kernel
 fi
 cd /opt/kernel
+cp include/linux/compiler-gcc5.h include/linux/compiler-gcc8.h
+cat /opt/PowerOS/config/config.chromeos /opt/PowerOS/config/config.chromeos.extra > .config
+cp /opt/wireless-regdb/db.txt /opt/kernel/net/wireless
+make oldconfig
+make prepare
+make CFLAGS="-O2 -s" -j$(nproc) Image
+make CFLAGS="-O2 -s" -j$(nproc) modules
+make dtbs
+make CFLAGS="-O2 -s" -j$(nproc)
