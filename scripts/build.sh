e25e80859010268f161863cf78754c3257552471
buzzy <daniele.testa@gmail.com>
2019-07-14 01:22:59 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,6 +25,7 @@ if [ ! -d "/opt/kernel" ]; then
   tar xfv /opt/kernel.tar.gz -C /opt/kernel
 fi
 cd /opt/kernel
+patch -p1 < /opt/PowerOS/patches/linux-3.18-log2.patch
 patch -p1 < /opt/PowerOS/patches/linux-3.18-hide-legacy-dirs.patch
 cp include/linux/compiler-gcc5.h include/linux/compiler-gcc8.h
 cat /opt/PowerOS/config/config.chromeos /opt/PowerOS/config/config.chromeos.extra > .config
