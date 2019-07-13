85ec52f20d43cbe3de507336566fab65cc31b326
buzzy <daniele.testa@gmail.com>
2019-07-14 01:18:47 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,14 +38,14 @@ make CFLAGS="-O2 -s" -j$(nproc)
 
 make INSTALL_MOD_PATH="/tmp/modules" modules_install
 rm -f /tmp/modules/lib/modules/*/{source,build}
-mkdir -p /opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/modules
-cp -rv /tmp/modules/lib/modules/3.18.0-19095-g86596f58eadf/* /opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/modules
-ln -s 3.18.0-19095-g86596f58eadf /opt/sysroot/Programs/linux-kernel/current
-ln -s /Programs/linux-kernel/3.18.0-19095-g86596f58eadf/modules /opt/sysroot/System/Kernel/Modules/3.18.0-19095-g86596f58eadf
+mkdir -p /opt/sysroot/Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/modules
+cp -rv /tmp/modules/lib/modules/3.18.0-19095-g86596f58eadf/* /opt/sysroot/Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/modules
+ln -s 3.18.0-19095-g86596f58eadf /opt/sysroot/Programs/linux-kernel-aarch64/current
+ln -s /Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/modules /opt/sysroot/System/Kernel/Modules/3.18.0-19095-g86596f58eadf
 rm -rf /tmp/modules
 #depmod -b /opt/sysroot/System/Kernel/Modules -F System.map "3.18.0-19095-g86596f58eadf"
 
-make INSTALL_DTBS_PATH="/opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/dtbs" dtbs_install
+make INSTALL_DTBS_PATH="/opt/sysroot/Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/dtbs" dtbs_install
 
 cp /opt/PowerOS/signing/kernel.its .
 mkimage -D "-I dts -O dtb -p 2048" -f kernel.its vmlinux.uimg
@@ -53,16 +53,16 @@ dd if=/dev/zero of=bootloader.bin bs=512 count=1
 echo "console=tty1 init=/sbin/init root=PARTUUID=%U/PARTNROFF=1 rootwait rw noinitrd" > cmdline
 vbutil_kernel --pack vmlinux.kpart --version 1 --vmlinuz vmlinux.uimg --arch aarch64 --keyblock /opt/PowerOS/signing/kernel.keyblock --signprivate /opt/PowerOS/signing/kernel_data_key.vbprivk --config cmdline --bootloader bootloader.bin
 mkdir -p /opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/image
-cp vmlinux.kpart /opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/image
+cp vmlinux.kpart /opt/sysroot/Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/image
 
 make mrproper
 make ARCH=arm headers_check
 make ARCH=arm INSTALL_HDR_PATH="/tmp/headers" headers_install
-mkdir -p /opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/headers
-cp -rv /tmp/headers/include/* /opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/headers
+mkdir -p /opt/sysroot/Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/headers
+cp -rv /tmp/headers/include/* /opt/sysroot/Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/headers
 rm -fr /tmp/headers
 #link headers to include dir??
-find /opt/sysroot/Programs/linux-kernel/3.18.0-19095-g86596f58eadf/headers \( -name .install -o -name ..install.cmd \) -delete
+find /opt/sysroot/Programs/linux-kernel-aarch64/3.18.0-19095-g86596f58eadf/headers \( -name .install -o -name ..install.cmd \) -delete
 
 #BUSYBOX:
 export ARCH=arm
