01d32fbadbee7e087fb4232894c4b229aff27db7
buzzy <daniele.testa@gmail.com>
2019-07-15 00:13:22 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -229,3 +229,123 @@ rm -rf /opt/sysroot/Programs/bison/3.4.1/share/{bison,doc,info,man}
 link_files /System/Index/Binaries /Programs/bison/3.4.1/bin
 link_files /System/Index/Shared /Programs/bison/3.4.1/share
 
+#flex
+cd /opt
+wget https://github.com/westes/flex/files/981163/flex-2.6.4.tar.gz
+tar xfv flex-2.6.4.tar.gz
+cd flex-2.6.4
+sed -i "/math.h/a #include <malloc.h>" src/flexdef.h
+
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --disable-static
+
+make -j$(nproc)
+make install DESTDIR=/opt/sysroot/Programs/flex/2.6.4
+ln -s 2.6.4 /opt/sysroot/Programs/flex/current
+rm -rf /opt/sysroot/Programs/flex/2.6.4/share
+
+link_files /System/Index/Binaries /Programs/flex/2.6.4/bin
+link_files /System/Index/Includes /Programs/flex/2.6.4/include
+link_files /System/Index/Libraries /Programs/flex/2.6.4/lib
+
+#make
+cd /opt
+wget http://ftp.twaren.net/Unix/GNU/gnu/make/make-4.2.1.tar.gz
+tar xfv make-4.2.1.tar.gz
+cd make-4.2.1
+sed -i '211,217 d; 219,229 d; 232 d' glob/glob.c
+
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --prefix=/ \
+  --host=arm-linux-gnueabihf
+
+make -j$(nproc)
+make install DESTDIR=/opt/sysroot/Programs/make/4.2.1
+ln -s 4.2.1 /opt/sysroot/Programs/make/current
+rm -rf /opt/sysroot/Programs/make/4.2.1/share
+
+link_files /System/Index/Binaries /Programs/make/4.2.1/bin
+link_files /System/Index/Includes /Programs/make/4.2.1/include
+
+#m4
+cd /opt
+wget https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.xz
+tar xfv m4-1.4.18.tar.xz
+cd m4-1.4.18
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --prefix=/
+
+make -j$(nproc)
+make install DESTDIR=/opt/sysroot/Programs/m4/1.4.18
+ln -s 1.4.18 /opt/sysroot/Programs/m4/current
+rm -rf /opt/sysroot/Programs/m4/1.4.18/share
+
+link_files /System/Index/Binaries /Programs/m4/1.4.18/bin
+
+#pkg-config
+cd /opt
+wget https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
+tar xfv pkg-config-0.29.2.tar.gz
+cd pkg-config-0.29.2
+
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --prefix=/ \
+  --with-internal-glib \
+  --disable-host-tool \
+  glib_cv_stack_grows=yes \
+  glib_cv_uscore=no \
+  ac_cv_func_posix_getpwuid_r=yes \
+  ac_cv_func_posix_getgrgid_r=yes
+
+make -j$(nproc)
+make install DESTDIR=/opt/sysroot/Programs/pkg-config/0.29.2
+ln -s 0.29.2 /opt/sysroot/Programs/pkg-config/current
+rm -rf /opt/sysroot/Programs/pkg-config/0.29.2/share/{doc,man}
+
+link_files /System/Index/Binaries /Programs/pkg-config/0.29.2/bin
+link_files /System/Index/Shared /Programs/pkg-config/0.29.2/share
+
+#libnl (netlink)
+cd /opt
+wget https://github.com/thom311/libnl/releases/download/libnl3_4_0/libnl-3.4.0.tar.gz
+tar xfv libnl-3.4.0.tar.gz
+cd libnl-3.4.0
+
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --sysconfdir=/etc \
+  --disable-cli \
+  --disable-static
+  
+make -j$(nproc)
+make install DESTDIR=/opt/sysroot/Programs/libnl/3.4.0
+ln -s 3.4.0 /opt/sysroot/Programs/libnl/current
+rm -rf /opt/sysroot/Programs/libnl/3.4.0/share/man
+mv /opt/sysroot/Programs/libnl/3.4.0/lib/pkgconfig /opt/sysroot/Programs/libnl/3.4.0/share
+
+link_files /System/Settings /Programs/libnl/3.4.0/etc
+link_files /System/Index/Includes /Programs/libnl/3.4.0/include
+link_files /System/Index/Libraries /Programs/libnl/3.4.0/lib
+link_files /System/Index/Shared /Programs/libnl/3.4.0/share
+
+#iw (tools for wifi)
+cd /opt
+wget https://www.kernel.org/pub/software/network/iw/iw-5.0.1.tar.xz
+tar xfv iw-5.0.1.tar.xz
+cd iw-5.0.1
+CC="arm-linux-gnueabihf-gcc --sysroot=/opt/sysroot" \
+PKG_CONFIG_PATH=/opt/sysroot/System/Index/Shared/pkgconfig \
+CFLAGS="--sysroot=/opt/sysroot -O2 -s -I/opt/sysroot/System/Index/Includes/libnl3" \
+LDFLAGS="-L/opt/sysroot/System/Index/Libraries -lnl-3" \
+
