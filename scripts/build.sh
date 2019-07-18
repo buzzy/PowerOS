244ad96a0936014d1859fd481a4f49513c40b97b
buzzy <daniele.testa@gmail.com>
2019-07-18 17:53:55 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -482,7 +482,8 @@ wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_p
 wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
 ./configure \
   CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
-  --host=arm-linux-gnueabihf \
+  LDFLAGS="-static" \
+  --host=aarch64-linux-gnu \
   --prefix=/
 make -j$(nproc)
 make install DESTDIR=/opt/sysroot/Programs/gobohide/0.14
