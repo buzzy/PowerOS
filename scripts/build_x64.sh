daa9fd6c1d18991ca44454f3bfb4f3d27411e380
buzzy <daniele.testa@gmail.com>
2019-07-22 23:05:58 +0800

    Update build_x64.sh

--- scripts/build_x64.sh
+++ scripts/build_x64.sh
@@ -9,7 +9,7 @@ link_files () {
 }
 
 #FETCH NEEDED TOOLS
-apt-get install -y gcc-8-aarch64-linux-gnu gcc-8-arm-linux-gnueabihf gawk bison wget patch build-essential u-boot-tools bc vboot-kernel-utils libncurses5-dev g++-arm-linux-gnueabihf flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev
+apt-get install -y gawk bison wget patch build-essential u-boot-tools bc vboot-kernel-utils libncurses5-dev flex texinfo unzip help2man libtool-bin python3 git nano kmod pkg-config autogen autopoint gettext libnl-cli-3-dev
 
 #CREATE DIR STRUCTURE
 rm -fr /opt/sysroot/*
@@ -21,9 +21,6 @@ git clone git://git.kernel.org/pub/scm/linux/kernel/git/linville/wireless-regdb.
 
 #KERNEL
 cd /opt
-ln -s /usr/bin/aarch64-linux-gnu-gcc-8 /usr/bin/aarch64-linux-gnu-gcc
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
 export WIFIVERSION=
 if [ ! -d "/opt/kernel" ]; then
   wget -O /opt/kernel.tar.gz https://chromium.googlesource.com/chromiumos/third_party/kernel/+archive/86596f58eadf.tar.gz
